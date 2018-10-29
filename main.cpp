d1d9b45211c26cecca59f06c3260950a7b115fe6
Nikolai <dancingdar@mail.ru>
2018-10-30 00:06:31 +0300

    Best presentation

--- main.cpp
+++ main.cpp
@@ -52,17 +52,19 @@ int main() {
 
 /*====================================================================================================================*/
 
-    int count_iter;
-    double count_iter_d = 0,
-            fractpart,                   // дробная часть
-            intpart;                     // целая часть;
+    int stop_cr;
+    double stop_cr_d = 0;
+    std::cout << "Choose stop criterion:\n"
+              << "< 1 > Maximum iterations.\n"
+              << "< 2 > Number of iterations after last improvement.\n"
+              << "< 3 > |f(x_{k+j}) - f(x_{k})| < epsilon\n" << std::endl;
 
-    std::cout << "\nMaximum number of iterations:\n";
-    while (count_iter_d <= 0 || modf(count_iter_d, &intpart) != 0) {
-        std::cout << "Please , enter a positive integer\n" << "> ";
-        std::cin >> count_iter_d;
-        if (!(count_iter_d <= 0 || modf(count_iter_d, &intpart) != 0)) {
-            count_iter = (int) count_iter_d;
+    while (!(stop_cr_d == 1 || stop_cr_d == 2 || stop_cr_d == 3)) {
+        std::cout << "Please , enter a number (1, 2, 3)\n";
+        std::cout << "> ";
+        std::cin >> stop_cr_d;
+        if ((stop_cr_d == 1 || stop_cr_d == 2 || stop_cr_d == 3)) {
+            stop_cr = (int) stop_cr_d;
             break;
         }
         std::cin.clear();
@@ -73,70 +75,193 @@ int main() {
 
 /*====================================================================================================================*/
 
-    double epsilon = 0;
-    std::cout << "epsilon: " << std::endl;
-    std::cin >> epsilon;
-    std::cout << std::endl;
+    int count_iter;
+    double count_iter_d = 0,
+            fractpart,                   // дробная часть
+            intpart;                     // целая часть;
 
-    Options options(count_iter, epsilon);
+/*====================================================================================================================*/
+
+    Options *options;
+    Stop *stop;
+    switch (stop_cr) {
+        case 1: {
+            std::cout << "\nEnter maximum number of iterations:\n";
+            while (count_iter_d <= 0 || modf(count_iter_d, &intpart) != 0) {
+                std::cout << "Please , enter a positive integer\n" << "> ";
+                std::cin >> count_iter_d;
+                if (!(count_iter_d <= 0 || modf(count_iter_d, &intpart) != 0)) {
+                    count_iter = (int) count_iter_d;
+                    break;
+                }
+                std::cin.clear();
+                std::string line;
+                std::getline(std::cin, line);
+                std::cout << "\nUnfortunately, your input is not a positive integer. Try again..." << std::endl;
+            }
+
+            options = new Options(count_iter);
+            stop = new StopCriterion_1();
+            break;
+        }
+        case 2: {
+            std::cout << "\nEnter maximum number of iterations:\n";
+            while (count_iter_d <= 0 || modf(count_iter_d, &intpart) != 0) {
+                std::cout << "Please , enter a positive integer\n" << "> ";
+                std::cin >> count_iter_d;
+                if (!(count_iter_d <= 0 || modf(count_iter_d, &intpart) != 0)) {
+                    count_iter = (int) count_iter_d;
+                    break;
+                }
+                std::cin.clear();
+                std::string line;
+                std::getline(std::cin, line);
+                std::cout << "\nUnfortunately, your input is not a positive integer. Try again..." << std::endl;
+            }
+
+            options = new Options(count_iter);
+            stop = new StopCriterion_2();
+            break;
+        }
+        case 3: {
+            double epsilon;
+            std::cout << "Enter epsilon.\n";
+            epsilon = 0;
+            while (epsilon >= 1 || epsilon <= 0) {
+                std::cout << "Please , enter 0 < epsilon < 1\n" << "> ";
+                std::cin >> epsilon;
+                if (!(epsilon <= 0 || epsilon >= 1)) {
+                    break;
+                }
+                std::cin.clear();
+                std::string line;
+                std::getline(std::cin, line);
+                std::cout << "\nUnfortunately, your input is wrong. Try again..." << std::endl;
+            }
+            options = new Options(epsilon);
+            stop = new StopCriterion_3();
+            break;
+        }
+        default: {
+            break;
+        }
+    }
+/*====================================================================================================================*/
+
+
 
     int dim = function->get_dim();
     std::vector<double> res(dim);
-    std::cout << "Выберите метод оптимизации" << std::endl;
-    StopCriterion_1 stop;
+
     int method;
-    std::cin >> method;
+    double method_d = 0;
+
+    std::cout << "Choose method, please:\n"
+              << "< 1 > Random search.\n"
+              << "< 2 > Newton by direction.\n" << std::endl;
+
+    while (!(method_d == 1 || method_d == 2)) {
+        std::cout << "Please , enter a number (1, 2)\n";
+        std::cout << "> ";
+        std::cin >> method_d;
+        if ((method_d == 1 || method_d == 2)) {
+            method = (int) method_d;
+            break;
+        }
+        std::cin.clear();
+        std::string line;
+        std::getline(std::cin, line);
+        std::cout << "\nUnfortunately, your input is not 1 or 2. Try again..." << std::endl;
+    }
 
     Optimization opt;
 
-    if (method == 1) {
-        double delta = 0;
-        std::cout << "delta: " << std::endl;
-        std::cin >> delta;
-        std::cout << std::endl;
+    switch (method) {
+        case 1: {
+            double p;
+            std::cout << "Enter probability.\n";
+            p = 0;
+            while (p >= 1 || p <= 0) {
+                std::cout << "Please , enter 0 < p < 1\n" << "> ";
+                std::cin >> p;
+                if (!(p <= 0 || p >= 1)) {
+                    break;
+                }
+                std::cin.clear();
+                std::string line;
+                std::getline(std::cin, line);
+                std::cout << "\nUnfortunately, your input is wrong. Try again..." << std::endl;
+            }
 
-        double p;
-        std::cout << "probability p: " << std::endl;
-        std::cin >> p;
+            double delta;
+            std::cout << "Enter delta.\n";
+            delta = 0;
+            while (delta >= 1 || delta <= 0) {
+                std::cout << "Please , enter 0 < delta < 1\n" << "> ";
+                std::cin >> delta;
+                if (!(delta <= 0 || delta >= 1)) {
+                    break;
+                }
+                std::cin.clear();
+                std::string line;
+                std::getline(std::cin, line);
+                std::cout << "\nUnfortunately, your input is wrong. Try again..." << std::endl;
+            }
 
-        double point;
+            std::vector<double> left_bound, right_bound;
+            std::cout << "Enter left bound, please\n" << std::endl;
+            double dot = 0;
+            for (int i = 0; i < dim; ++i) {
+                while (!(std::cin >> dot)) {
+                    std::cout << "Ooops...\n";
+                }
+                left_bound.push_back(dot);
+            }
 
-        std::vector<double> left_bound, right_bound;
-        std::cout << "left bound:" << std::endl;
-        for (int i = 0; i < dim; ++i) {
-            std::cin >> point;
-            left_bound.push_back(point);
+            std::cout << "Enter right bound, please\n" << std::endl;
+            dot = 0;
+            for (int i = 0; i < dim; ++i) {
+                while (!(std::cin >> dot)) {
+                    std::cout << "Ooops...\n";
+                }
+                right_bound.push_back(dot);
+            }
+
+            Rectangle rectangle(left_bound, right_bound, dim);
+
+            rectangle.set_delta(delta);
+
+            res = opt.optimizationRa(function, rectangle, stop, options, p);
+            break;
         }
+        case 2: {
+            std::vector<double> vector;
+            std::cout << "Enter start point, please\n" << std::endl;
+            double dot = 0;
+            for (int i = 0; i < dim; ++i) {
+                while (!(std::cin >> dot)) {
+                    std::cout << "Ooops...\n";
+                }
+                vector.push_back(dot);
+            }
 
-
-        std::cout << "right bound:" << std::endl;
-        for (int i = 0; i < dim; ++i) {
-            std::cin >> point;
-            right_bound.push_back(point);
+            res = opt.optimizationNe(vector, function, stop, options);
+            break;
         }
-
-        Rectangle rectangle(left_bound, right_bound, dim);
-
-        rectangle.set_delta(delta);
-
-        res = opt.optimizationRa(function, rectangle, stop, options, p);
-    } else {
-        std::vector<double> x(dim);
-        double t;
-        std::cout << "point start" << std::endl;
-        for (int i = 0; i < dim; ++i) {
-            std::cin >> t;
-            x[i] = t;
+        default: {
+            break;
         }
-        res = opt.optimizationNe(x, function, stop, options);
     }
-    std::cout << "minimum point: ";
+
+    std::cout << "Point of minimum: ";
     for (int i = 0; i < dim; ++i) {
         std::cout << res[i];
         std::cout << " ";
     }
     std::cout << std::endl;
 
-    std::cout << "value: " << function->get_value(res) << std::endl;
+    std::cout << "Value: " << function->get_value(res) << std::endl;
+
+    std::cout << "Goodbye!" << std::endl;
     return 0;
 }
