fec98ef43af4707adaa70b2d6e645b5c9a49c5b5
Nikolai <dancingdar@mail.ru>
2018-11-16 00:00:12 +0300

    Comments update

--- Optimization.h
+++ Optimization.h
@@ -14,8 +14,10 @@
 /*====================================================================================================================*/
 
 /**
- * Абстрактный класс оптимизации.
- * Будет иметь двух наслдеников.
+ * Класс методов оптимизации.
+ * Есть два метода:
+ * <1> Случайный поиск --- optimizationRandomSearch
+ * <2> Метод Ньютона по направлению --- optimizationNewton
  */
 class Optimization {
 protected:
@@ -25,13 +27,22 @@ protected:
     std::uniform_real_distribution<> _dist;
 public:
     Optimization();
+    /*
+     * Функция случайного поиска.
+     */
     std::vector<double>
-    optimizationRa(Function *function, Rectangle &rectangle, Stop *stop, Options *options, double p);
+    optimizationRandomSearch(Function *function, Rectangle &rectangle, Stop *stop, Options *options, double p);
 
+    /*
+     * Функция возвращает случайную точку из области.
+     */
     std::vector<double> rand_vector(Rectangle &area);
 
+    /*
+     * Метод Ньютона по направлению.
+     */
     std::vector<double>
-    optimizationNe(std::vector<double> &point, Function *function, Stop *stop, Options *options);
+    optimizationNewton(std::vector<double> &point, Function *function, Stop *stop, Options *options, Rectangle &rectangle);
 
 
     std::vector<double> copy(std::vector<double> &v, VectorXd &x);
